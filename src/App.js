8664bb2218eb9ac00472c04c6a009dacb5c1c5ec
João Vitor Corrêa <70239873+joaovitorcorrea06@users.noreply.github.com>
2022-05-12 13:50:19 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,46 +1,83 @@
 import './App.css';
 import axios from 'axios';
 import { listarLog } from './listar-log';
-import { Form, Col, Table } from 'react-bootstrap';
+import { Form, Col, Table, Container, Dropdown } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
 
+  listarLog.sort(function (a,b){
+    return (new Date(b.data) - new Date(a.data));
+  });
 
-  // function mostrarLog(){
-  //   axios.get('log.json')
-  //   .then(res=> res.data.map(function(dado){
-  //     return (
-  //     <li>{dado.mensagem}</li>)}))
-  //   .catch(err=>alert('nao peguei'))
-  // }
+  const [filtro, setfiltro] = useState('TODOS');
+  const [Dados, setDados] = useState(listarLog);
+
+  function busca(dado) {
+    const res = listarLog.map(item)
+    if (item.type = dado){
+      return item;
+    }
+    setDados(res)
+
+  }
+     // for each
 
   return (
-    <div>
-      <h1> Log </h1>
-      <Table>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Data</th>
-          <th>Hora</th>
-          <th>Mensagem</th>
-        </tr>
-      </thead>
-        {listarLog.map(function(dado){
-          return (
-            <tbody>
-            <tr>
-              <td>{dado.type}</td>
-              <td>{dado.data}</td>
-              <td>{dado.time}</td>
-              <td>{dado.mensagem}</td>
-            </tr>
-          </tbody> 
-        )})} 
+
+    <Container fluid>
+      <div>
+        <h1> Log </h1>
+        <br></br>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            FILTRO
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={()=>{
+              setfiltro('ERRO')
+            }} >ERRO</Dropdown.Item>
+              
+            <Dropdown.Item onClick={()=>{
+              setfiltro ('INFO')
+            }}>INFO</Dropdown.Item>
+
+            <Dropdown.Item onClick={()=>{
+              setfiltro ('DEBUG')
+            }}>DEBUG</Dropdown.Item>
+
+          </Dropdown.Menu>
+        </Dropdown>
+        <br></br>
+
+        <Table bordered hover>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Data</th>
+            <th>Hora</th>
+            <th>Mensagem</th>
+          </tr>
+        </thead>
+          {Dados.map(function(dado){
+            return (
+              <tbody>
+              <tr>
+                <td>{dado.type}</td>
+                <td>{dado.data}</td>
+                <td>{dado.time}</td>
+                <td>{dado.mensagem}</td>
+              </tr>
+            </tbody> 
+          )})} 
+          
+        </Table>
+
+        <p>teste {filtro}</p>
         
-      </Table>
-      
-    </div>
+      </div>
+    </Container>
   );
 }
 
--- src/listar-log.js
+++ src/listar-log.js
@@ -1,5 +1,5 @@
 
-export const listarLog =  
+export let listarLog =  
     [
       {
           "data": "2022-05-01",
@@ -18,19 +18,51 @@ export const listarLog =
           "time": "08:07:00",
           "type": "ERRO",
           "mensagem": "minha mensagem de teste"
+      },
+      {
+          "data": "2022-05-05",
+          "time": "08:07:00",
+          "type": "ERRO",
+          "mensagem": "minha mensagem de teste"
+      },
+      {
+          "data": "2022-05-07",
+          "time": "08:07:00",
+          "type": "ERRO",
+          "mensagem": "minha mensagem de teste"
       }
   ];
 
-  function dataFormat(d){
+//   function dateFormat(d){
    
-    var d_split = d.split("/");
-    var dia = d_split[0];
-    var mes = d_split[1]-1;
-    var ano = d_split[2];
+//     var d_split = d.split("-");
+//     var ano = d_split[0];
+//     var mes = d_split[1]-1;
+//     var dia = d_split[2];
  
-    return [dia,mes,ano];
- }
+//     return [dia,mes,ano];
+//  }
 
-  // listarLog.sort(function(a,b)) {
-  //   return a.dia < b.dia ? -1 : a.dia > b.dia ? 1 : 0
-  // }
+//   dateFormat(listarLog).sort(function(a,b) {
+//     return a.dia < b.dia 
+//   });
+
+
+
+//     listarLog.sort(function(a, b) {
+//     // aqui vai converter "data" para um object Date
+//     // a seguir "vê qual é maior"
+//     return (new Date(a.data) - new Date(b.data));
+//   });
+
+//   console.log(listarLog);
+
+
+
+
+// function ordemDecrescente(a, b) {
+//     return a.data < b.data;
+// }
+
+// listarLog.sort(ordemDecrescente);
+  
